a8944e3124baa113a0b9214db0b1b55a56719e8a
= <thorchr96@gmail.com>
2023-01-29 09:14:21 +0100

    Think it SHOULD work.

---
+++ RoundToSignificantFactor/RoundToSignificantFactor.Library/RoundSigFigs.cs
@@ -0,0 +1,24 @@
+﻿namespace RoundToSignificantFactor.Library;
+
+public static class RoundSigFigs
+{
+    public static double RoundToSignificantFactors(double num, int n)
+    {
+        if (n < 1)
+        {
+            throw new ArgumentException("n must be greater than or equal to 1");
+        }
+
+        if (num == 0)
+        {
+            return 0;
+        }
+
+        double d = Math.Ceiling(Math.Log10(num < 0 ? -num : num));
+        int power = n - (int)d;
+
+        double magnitude = Math.Pow(10, power);
+        double shifted = Math.Round(num * magnitude);
+        return shifted / magnitude;
+    }
+}
---
+++ RoundToSignificantFactor/RoundToSignificantFactor.UnitTests/RoundSigFigsTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoundToSignificantFactor.Library;
+
+namespace RoundToSignificantFactor.UnitTests;
+
+
+[TestClass]
+public class RoundSigFigsTests
+{
+    [TestMethod]
+    public void TestRoundSigFigs_PositiveNumber()
+    {
+        double num = 123.4567;
+        int n = 4;
+        double expected = 123.5;
+        double result = RoundSigFigs.RoundToSignificantFactors(num, n);
+        Assert.AreEqual(expected, result, 0.0001);
+    }
+
+    [TestMethod]
+    public void TestRoundSigFigs_NegativeNumber()
+    {
+        double num = -123.4567;
+        int n = 4;
+        double expected = -123.5;
+        double result = RoundSigFigs.RoundToSignificantFactors(num, n);
+        Assert.AreEqual(expected, result, 0.0001);
+    }
+    [TestMethod]
+    public void TestRoundSigFigs_ZeroNumber()
+    {
+        double num = 0.0;
+        int n = 4;
+        double expected = 0.0;
+        double result = RoundSigFigs.RoundToSignificantFactors(num, n);
+        Assert.AreEqual(expected, result, 0.0001);
+    }
+    [TestMethod]
+    public void TestRoundSigFigs_NumberWithLessSigFigs()
+    {
+        double num = 12.3;
+        int n = 4;
+        double expected = 12.3;
+        double result = RoundSigFigs.RoundToSignificantFactors(num, n);
+        Assert.AreEqual(expected, result);
+    }
+    [TestMethod]
+    public void TestRoundSigFigs_NumberWithMoreSigFigs()
+    {
+        double num = 12.3456789;
+        int n = 4;
+        double expected = 12.35;
+        double result = RoundSigFigs.RoundToSignificantFactors(num, n);
+        Assert.AreEqual(expected, result, 0.0001);
+    }
+}
